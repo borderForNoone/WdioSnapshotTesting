3784fc0f2bf5bc4e0aa29f21fc3a21b76bf2b2b5
Denys <deniska2002kulikov@gmail.com>
2025-06-16 09:47:47 +0300

    fixed comments

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -2,21 +2,19 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-// describe('My Login application', () => {
-//     it('should login with valid credentials', async () => {
-//         await LoginPage.open();
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
 
-//         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-//         await expect(SecurePage.flashAlert).toBeExisting();
-//         await expect(SecurePage.flashAlert).toHaveText(
-//             expect.stringContaining('You logged into a secure area!')
-//         );
-//         await expect(await $('#flash').getText()).toMatchSnapshot();
-//         await expect(SecurePage.flashAlert).toMatchElementSnapshot('flashAlert');
-//     });
-// });
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveText(
+            expect.stringContaining('You logged into a secure area!')
+        );
+        await expect(await $('#flash').getText()).toMatchSnapshot();
+        await expect(SecurePage.flashAlert).toMatchElementSnapshot('flashAlert');
+    });
 
-describe('Visual Examples', ()=> {
     it('should be able to create a fullpage screenshot', async () => {
         await browser.url('http://guinea-pig.webdriver.io/image-compare.html');
         await $('.hero__title-logo').waitForDisplayed();
@@ -26,5 +24,4 @@ describe('Visual Examples', ()=> {
             hideAfterFirstScroll: [await $('nav')]
         });
     });
-})
-
+});
